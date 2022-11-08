0c347773f320fd1a983c2d79bf66a27e1c120789
Lucy de Rojas <lucyderojas@gmail.com>
2022-11-08 14:15:33 +0000

    styling main layout

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,10 @@
 import Logo from "./Logo"
 import MainNavigation from "./MainNavigation"
 import BottomLinks from "./BottomLinks"
+import RightNavigation from "./RightNavigation"
 
 
+import styles from '../styles/layout.module.css';
 
 
 export default function Layout ({children}) {
@@ -13,11 +15,19 @@ export default function Layout ({children}) {
 
     return (<div>
         <Logo/>
+
+
+{/* 3 column flex box */}
+        <div className={styles.content}>
         <MainNavigation />
 
 
 
 {children}
+
+<RightNavigation />
+        </div>
+{/* end of 3 column flex box */}
 
 <BottomLinks />
     </div>)
---
+++ components/RightNavigation.js
@@ -0,0 +1,46 @@
+﻿
+import Link from "next/link";
+import styles from '../styles/mainNavigation.module.css';
+
+
+
+export default function RightNavigation () {
+
+    let items = ['About','Upcoming Events','Meet the Ladies Team','Meet the Man`s Team', 'Join Us','Contact'];
+
+    let links = ['about','upcomingEvents','meetLadiesTeam','meetManTeam', 'joinUs','contact'];
+
+
+
+    return (<div className={styles.navigation}>
+        {
+            items.map((item, index)=> {
+                return <Link href={links[index]}><button key={index} className={styles.button}>{item}</button>
+                </Link>
+            })
+        }
+
+    </div>);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
