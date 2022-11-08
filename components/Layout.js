5c5c91489bd2cfc429c284984a8274afba299c7b
Lucy de Rojas <lucyderojas@gmail.com>
2022-11-08 12:07:45 +0000

    setup

---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+﻿
+import Logo from "./Logo"
+
+
+
+
+
+
+export default function Layout ({children}) {
+
+
+
+
+    return (<div>
+        <Logo/>
+
+<h1>Layout</h1>
+
+
+{children}
+
+
+    </div>)
+}
---
+++ components/Logo.js
@@ -0,0 +1,38 @@
+﻿
+
+import Image from "next/image";
+import Link from "next/link";
+import styles from '../styles/logo.module.css';
+
+export default function Logo () {
+
+
+    return (<div className={styles.logo}>
+
+<Link href='/'>
+
+
+
+
+
+
+
+
+
+
+        <Image
+
+            src= '/Media/images/logo.svg'
+            width='1559'
+            height='850'
+            alt="logo"
+            layout="responsive"
+
+
+        />
+
+</Link>
+
+
+    </div>);
+}
