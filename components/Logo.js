d985a8779fd08bed33ae14dc71e571de7b0da135
Lucy de Rojas <lucyderojas@gmail.com>
2022-11-15 14:32:09 +0000

    LOGO -> centered

--- components/Logo.js
+++ components/Logo.js
@@ -7,32 +7,27 @@ import styles from '../styles/logo.module.css';
 export default function Logo () {
 
 
-    return (<div className={styles.logo}>
+    return (<div className={styles.wrapper}>
+
+
+<div className={styles.logo}>
 
 <Link href='/'>
 
-
-
-
-
-
-
-
-
-
         <Image
 
-            src= '/Media/images/logo.png'
-            width='1559'
-            height='850'
-            alt="logo"
-            layout="responsive"
+src= '/Media/images/logo.png'
+width='1559'
+height='850'
+alt="naphill tennis club logo"
+layout="responsive"
 
 
-        />
+/>
 
 </Link>
 
+</div>
 
     </div>);
 }
