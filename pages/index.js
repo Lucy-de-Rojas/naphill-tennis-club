6fbb9ce7cf357ee951f56ed21d023bd08eed57bc
Lucy de Rojas <lucyderojas@gmail.com>
2022-11-15 13:59:10 +0000

    fixing fiotting

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,57 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+
+
+import {useState, useEffect} from 'react';
 
 
 
 
 export default function Home() {
+
+
+console.log('hello home page');
+
+  const [data, setData] = useState({});
+
+
+  useEffect(()=> {
+
+
+    async function getData() {
+
+      const response = await fetch('api/home');
+      const data = await response.json();
+      setData(data);
+      console.log('data:>>>',data);
+
+    }
+
+
+
+    getData();
+
+
+  },[]);
+
+
+
+
+
+
+
+
   return (
     <Layout>
 
 
-<h1>Home page</h1>
+<h1>{data.title}</h1>
+<h2>{data.subhead}</h2>
+<p>{data.mainInfo}</p>
+
+
 
 
     </Layout>
