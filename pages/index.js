63fad3bac018bbe5463c0f7db35d5bccb792b3b1
Lucy de Rojas <lucyderojas@gmail.com>
2022-11-15 14:58:30 +0000

    HOME PAGE -> added previous events images

--- pages/index.js
+++ pages/index.js
@@ -51,9 +51,55 @@ console.log('hello home page');
 <h1>{data.title}</h1>
 <h2>{data.subhead}</h2>
 <p>{data.mainInfo}</p>
+
+<h2>What is Tennis?</h2>
 <video width="100%" controls>
   <source src='/Media/videos/What-is-Tennis-by-Lucy-de-Rojas.mp4' type='video/mp4' ></source>
 </video>
+
+
+<h2>Previous Events:</h2>
+
+
+
+
+
+
+
+
+<div className={styles.image}>
+
+  <p>Barn Dance November 2016</p>
+
+<Image
+
+src="/Media/images/barnDance2016.jpg"
+width='509'
+height='720'
+alt='Naphill tennis club barn dance november 2016'
+layout='responsive'
+
+/>
+
+<br />
+<br />
+<br />
+<br />
+<br />
+<p>Tennis ball machine challenge June 2017</p>
+<Image
+
+src="/Media/images/tennisBallMachineChallenge.jpg"
+width='509'
+height='720'
+alt='Naphill tennis club barn dance november 2016'
+layout='responsive'
+
+/>
+
+
+
+</div>
 
 
 
