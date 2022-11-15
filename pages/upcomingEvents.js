0e8be0497d43acc7068a57859eb4fdca0cd85fa6
Lucy de Rojas <lucyderojas@gmail.com>
2022-11-15 14:00:05 +0000

    fixed fitting

--- pages/upcomingEvents.js
+++ pages/upcomingEvents.js
@@ -1,13 +1,41 @@
-﻿
+﻿import {useEffect, useState} from 'react';
+
+
+
 import Layout from "../components/Layout"
 
 
 export default function UpcomingEvents () {
 
+    const [dataWeb, setDataWeb] = useState({});
+
+
+
+    useEffect(()=> {
+
+        async function getData() {
+            let response = await fetch('api/upcoming-events');
+
+            let data = await response.json();
+
+            setDataWeb(data);
+
+            console.log('data: >>>',data);
+
+        }
+
+
+        getData();
+
+
+
+
+    },[]);
+
 
     return (<div>
 <Layout>
-<h1>Upcoming events</h1>
+<h1>{dataWeb.title}</h1>
 
 
 
