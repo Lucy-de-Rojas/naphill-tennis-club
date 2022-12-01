a0224bd75b27d1b993a21ce3401bd70fc9c10b6d
Lucy de Rojas <lucyderojas@gmail.com>
2022-12-01 13:35:04 +0000

    CONTACT page -> added form hook

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,13 @@
 ﻿import {useState, useEffect, use} from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
+import {useForm} from 'react-hook-form';
 
 
 import styles from '../styles/contact.module.css';
 import Layout from "../components/Layout"
+
+
 
 
 
@@ -17,8 +20,19 @@ import Layout from "../components/Layout"
 
 
 export default function Contact () {
-    console.clear();
+    // console.clear();
     console.log('loading contact page');
+    const formDefaults = {defaultValues: {
+        name:'',
+        email: '',
+        message: '',
+        timestamp: '',
+    }};
+
+
+
+    // using form hook:
+    const {register, handleSubmit, watch, formState: {errors}} = useForm(formDefaults);
 
 
 
@@ -28,13 +42,22 @@ export default function Contact () {
     
     
     
-    async function handleSubmit () {
+    async function handleSubmitFunc (data) {
         console.clear();
         let name = document.querySelector('#name').value;
         let email = document.querySelector('#email').value;
         let message = document.querySelector('#message').value;
 
-        let data = {
+
+     
+
+
+
+
+
+
+
+        let dataInput = {
             name: name,
             email:email,
             message:message,
@@ -65,7 +88,7 @@ export default function Contact () {
                     "Content-Type":"application/json",
                 },
                 
-                body: JSON.stringify(data),
+                body: JSON.stringify(dataInput),
 
             });
 
@@ -88,14 +111,7 @@ export default function Contact () {
         }
 
 
-
-
-
-
-
-
-
-
+        
 
 
 
@@ -115,22 +131,106 @@ export default function Contact () {
 
 
 
+<form onSubmit={handleSubmit(async(data) => {
+    console.clear();
+    console.log('submitting data from the from');
+    console.log('name:>>>',data.name);
+
+
+
+// sending data to API/Contact:
+    let response = await fetch('api/contact',{
+        method:'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            "Content-Type":"application/json",
+        },
+        
+        body: JSON.stringify(data),
+
+    });
+
+
+// redirecting to thank you page:
+        // sharing data:
+        Router.push({
+            pathname:'/contact-thank-you',
+            query: {
+                name: data.name,
+                email: data.email,
+                message: data.message,
+            },
+        });
+
+
+
+})}>
+
+
+{/* name: 👍 */}
+    <input {
+        ...register("name", {
+        required: {value: true, message: 'we do need your name'}, 
+        minLength: {value: 2, message: 'tooooo short'},
+        maxLength: {value: 100, message: 'tooooo long'},
+        
+    })}
+        type="text"
+        placeholder='name' />
+        <p>{errors.name?.message}</p>
+
+
+
+{/* email:  👍 */}
+
+<input {
+       ...register("email", {
+        required: {value: true, message: 'we do need your email'},
+        minLength: {value: 2, message: 'too short'},
+        maxLength: {value: 100, message: 'too long'},
+
+
+
+})}
+
+
+    placeholder='email'
+    type="email" />
+    <p>{errors.email?.message}</p>
+    
 
 
 
 
-{/* <form onSubmit={handleSubmit} action="contact-thank-you"> */}
-<div className={styles.wrapper}>
 
-    <input type='text' id="name" placeholder='name'/>
-    <input type='email' id="email" placeholder='email'/>
-    <textarea rows="5" id="message" placeholder='message'/>
+{/* message: */}
 
-    <input type="button" value="Submit" onClick={handleSubmit} />
-    {/* <input type="submit" value='Send Us A Message' /> */}
+    <textarea
+        {...register("message", {
+            required: {value: true, message: 'we do need your message'},
+            minLength: {value: 10, message: 'too short, 10 characters at least'},
+            maxLength: {value: 2000, message: 'too long'},
+        })}
+    
+    ></textarea>
+    <p>{errors.message?.message}</p>
 
-    </div>
-{/* </form> */}
+
+
+
+
+
+{/* submit: */}
+<input type="submit"/>
+
+</form>
+
+
+
+
+
  
 
 
