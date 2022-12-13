12780d9d22fb650abb109be686f847c3aea7f1f5
Lucy de Rojas <lucyderojas@gmail.com>
2022-12-13 14:00:50 +0000

    FAVICON added to all pages

--- components/Layout.js
+++ components/Layout.js
@@ -28,6 +28,9 @@ export default function Layout ({children}) {
 <script defer async src="https://kit.fontawesome.com/9f56af7d44.js" crossorigin="anonymous"></script>
 
 
+<link rel="shortcut icon" href="Naphill-Tennis-Club-Favocon.ico" />
+
+
 
 {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" /> */}
 
