3b1372ba7ab9bea2462ef0492767edf8a6f65619
Lucy de Rojas <lucyderojas@gmail.com>
2022-12-27 18:56:02 +0000

    adding cors for  api/hello

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import NextCors from "nextjs-cors"
 
-export default function handler(req, res) {
+
+export default async function handler(req, res) {
+
+  await NextCors(req, res, {
+    methods: ["GET","PUT"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  });
+
+
+
   res.status(200).json({ name: 'John Doe' })
 }
