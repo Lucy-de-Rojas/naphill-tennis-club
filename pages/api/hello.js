6e0ca71a3b725973ab58d2a9da7e5ea11aa0fa26
Lucy de Rojas <lucyderojas@gmail.com>
2022-12-28 00:30:46 +0000

    added middleware for all apis

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -11,5 +11,5 @@ export default async function handler(req, res) {
 
 allowCORS();
 
-  res.status(200).json({ name: 'John Doe', sender: "lucy de rojas" })
+  res.status(200).json({ name: 'John Doe', sender: "benji rojas" })
 }
