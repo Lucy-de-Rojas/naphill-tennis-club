21608021ea189149a2c00e18227b63903395b072
Lucy de Rojas <lucyderojas@gmail.com>
2022-12-28 00:44:24 +0000

    added middleware for all apis

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,13 +1,20 @@
 import NextCors from "nextjs-cors";
+import { allowCORS } from "../../libs/allowCors";
+
+
 
 export default async function handler(req, res) {
 
-  await NextCors(req, res, {
-    methods: ["GET"],
-    origin:"*",
-    optionsSuccessStatus: 200,
-});
 
 
-  res.status(200).json({ name: 'John Doe', sender: "benji rojas" })
+//   await NextCors(req, res, {
+//     methods: ["GET"],
+//     origin:"*",
+//     optionsSuccessStatus: 200,
+// });
+
+allowCORS(req, res);
+
+
+  res.status(200).json({sender: "benji rojas", reciever: 'mama' })
 }
