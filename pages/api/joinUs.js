7c12b70fcace9382a280c2e162d39ce861d4d068
Lucy de Rojas <lucyderojas@gmail.com>
2023-02-03 18:32:08 +0000

    database working for join us section

--- pages/api/joinUs.js
+++ pages/api/joinUs.js
@@ -7,9 +7,9 @@ import { mysqlConnect1 } from '../../utils/connectDB';
 
 
 export default async function handler(req, res) {
-    
+
     let data = req.body;
-    
+
     // testing node fetch:
     const mysqlDetails = await mysqlConnect1();
     console.log('mysql details :>>>', mysqlDetails)
@@ -17,24 +17,24 @@ export default async function handler(req, res) {
 
 
 
-    
+
     console.log('data from api:>>> ',data)
-    
+
     const connection = await mysql.createConnection(mysqlDetails);
 
     if(connection) {
         console.log('connected top MYSQL');
     }
 
-    
+
 
 
     // query:
     const query = `insert into membership (first_name, surname, email, phone, address, date_of_birth, membership, membershipSTART,membershipEND, membershipYEARS) values('${data.first_name}', '${data.surname}', '${data.email}', '${data.phone}', '${data.address}', '${data.date_of_birth}', '${data.membership}', '${data.membershipSTART}','${data.membershipEND}' ,'${data.membershipYEARS}')`;
-    
-    
-    
-    
+
+
+
+
     // executes insert:
     const values = [];
     const [results] = await connection.execute(query, values);
